324a7f27b7fb42435712e4ecc465c89bad84e443
Neeth Kunnath <neeth.xavier@gmail.com>
2024-07-09 17:05:23 -0400

    load pretrained weights

--- src/gpt2.py
+++ src/gpt2.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 
 class Config(BaseModel):
     block_size: int = 1024
-    voacb_size: int = 50257
+    vocab_size: int = 50257
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
@@ -79,9 +79,9 @@ class Block(nn.Module):
     def __init__(self, config: Config):
         super().__init__()
 
-        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
-        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
 
     def forward(self, x):
@@ -95,10 +95,58 @@ class GPT(nn.Module):
         super().__init__()
         self.transformer = nn.ModuleDict(
             dict(
-                wte=nn.Embedding(config.voacb_size, config.n_embd),
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
                 wpe=nn.Embedding(config.block_size, config.n_embd),
                 h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
                 ln_f=nn.LayerNorm(config.n_embd),
             )
         )
-        self.lm_head = nn.Linear(config.n_embd, config.voacb_size, bias=False)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model from huggingface"""
+
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+
+        gpt_config = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        gpt_config["block_size"] = 1024
+        gpt_config["vocab_size"] = 50257
+        from transformers import GPT2LMHeadModel
+
+        model = GPT(Config(**gpt_config))
+        sd = model.state_dict()
+        sd_keys = [k for k in sd.keys() if not k.endswith(".attn.bias")]  # discard this mask/buffer
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+
+        # basically the openai weights are Conv1D, but we only want to use a vanilla Linear
+        # this means maually transposing them when we import them
+        transposed = ["attn.c_attn.weight", "attn.c_proj.weight", "mlp.c_fc.weight", "mlp.c_proj.weight"]
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # transpose these Conv1D weights
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
+GPT.from_pretrained("gpt2")
