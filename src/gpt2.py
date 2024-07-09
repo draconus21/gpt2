0debee97500bef04f7cbae164367fc40da2dee4f
Neeth Kunnath <neeth.xavier@gmail.com>
2024-07-09 16:06:59 -0400

    block, MLP, and attn

--- src/gpt2.py
+++ src/gpt2.py
@@ -1,4 +1,7 @@
+import math
+import torch
 from torch import nn
+from torch.nn import functional as F
 from pydantic import BaseModel
 
 
@@ -8,6 +11,83 @@ class Config(BaseModel):
     n_layer: int = 6
     n_head: int = 6
     n_embd: int = 384
+
+
+class CausalSelfAttenion(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+
+        # projections for q, k, and v
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x):
+        B, T, C = x.size()
+
+        qkv = self.c_attn(x)  # (B, T, C*3)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+
+        # apply multi-head attention
+        # bring the head dim forward to be a batch dim (pytorch likes this)
+        # nh: number of heads, hs: head size, c: number of channels (hs*ns)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+
+        # compute attention
+        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))  # (B, nh, T, T)
+        # mask the future
+        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        attn = F.softmax(attn, dim=-1)
+        y = attn @ v  # (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # (B, nh, T, hs) -> (B, T, nh, hs) -> (B, T, C)
+
+        # ouput projection
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.MLP(self.ln2(x))
+        return x
 
 
 class GPT(nn.Module):
